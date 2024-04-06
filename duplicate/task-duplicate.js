78c767133043497a0b27ce6977c266e5e2fd39eb
cblupodev <chrisblupodev@gmail.com>
2024-04-06 08:13:44 -0400

    Create task-duplicate.js

---
+++ duplicate/task-duplicate.js
@@ -0,0 +1,22 @@
+const duplicateTagName = "+duplicate";
+let duplicateTheseTasks = rtm.getTasks(`tag:${duplicateTagName}`);
+
+duplicateTheseTasks.forEach(task => {
+    task.removeTags(duplicateTagName);
+    let duplicate = rtm.addTask(task.getName());
+    duplicate.setDueTime(task.getDueDate());
+    duplicate.setList(task.getList());
+    duplicate.setEstimate(task.getEstimate());
+    duplicate.setParent(task.getParent());
+    duplicate.setLocation(task.getLocation());
+    duplicate.setPriority(task.getPriority());
+    duplicate.setStartDate(task.getStartDate());
+    duplicate.setTags(...task.getTags().map(t => t.getName()));
+    duplicate.setUrl(task.getUrl());
+    if (task.getNotes().length > 0) {
+        task.getNotes().reverse().forEach(n => duplicate.addNote(n.getContent()));
+    }
+    if (task.isRecurring()) {
+        duplicate.setName(`${duplicate.getName()} (need to copy over the "repeats" field manually)`);
+    }
+});
